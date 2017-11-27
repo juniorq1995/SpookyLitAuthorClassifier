9b95875df5a344207fb9cf3a2adfd565c626687e
juniorq1995 <juniorq1995@users.noreply.github.com>
2017-11-26 20:07:59 -0600

    Update spooky.m

--- spooky.m
+++ spooky.m
@@ -2,5 +2,64 @@ filename='train.csv';
 trainDataTable = readtable(filename);
 trainDataArray = table2array(trainDataTable);
 
-% find features by taking the top 5 or 10 words each author uses and
+% 15 features composed of 3 different data types, each containing 5 entries
 % counting the frequency for each data element
+% 1.Want to find words that only each author uses (compare word to other 2
+% authors)
+% and words that they use 2.frequently and 3.infrequently
+
+%split at whitespace
+[length,~] = size(trainDataArray);
+EAP = [];
+HPL = [];
+MWS = [];
+for i = 1:100
+    str = string(trainDataArray(i,2));
+    %disp(i);
+    %remove punctuation
+    strn = regexprep(str,'[!@#$%^&*():;",.?<>/'']','');
+    
+    if(strcmp(trainDataArray(i,3), 'EAP') == 1)
+        EAP = [EAP lower(strsplit(strn))];
+    elseif(strcmp(trainDataArray(i,3), 'HPL') == 1)
+        HPL = [HPL lower(strsplit(strn))];
+    else
+        MWS = [MWS lower(strsplit(strn))];
+    end
+end
+
+[~,lengthEAP] = size(EAP);
+[~,lengthHPL] = size(HPL);
+[~,lengthMWS] = size(MWS);
+
+%Create unique vector with no repreating words, frequency data to be added
+%later on
+EAP_Final = unique(EAP);
+if(EAP_Final(1) == '' || EAP_Final == ' ')
+   EAP_Final =  EAP_Final(2:end);
+end
+[~,lengthEAP_Final] = size(EAP_Final);
+EAP_Final = [EAP_Final;zeros(1,lengthEAP_Final);zeros(1,lengthEAP_Final);zeros(1,lengthEAP_Final)];
+
+EAP = [EAP;zeros(1,lengthEAP);zeros(1,lengthEAP);zeros(1,lengthEAP)];
+for j = 1:lengthEAP
+    EAP(2,j) = sum(EAP(1,:) == EAP(1,j))/lengthEAP;
+end
+
+% Now that we have the frequency within it is time to look at the frequency
+% of a word one author uses relative to the other two authors. 
+% This fiqure is calculated by dividing the number of occurences of the word divided by
+% the number of elements nthe other two authors
+
+% Last figure is the frequency within divided by the frequency in others
+cmpEAP = [HPL MWS];
+for j = 1:lengthEAP
+    EAP(3,j) = sum(cmpEAP(1,:) == EAP(1,j))/(lengthHPL + lengthMWS);
+    numer = str2double(EAP(2,j));
+    denom = str2double(EAP(3,j));
+    if(denom == 0)
+        EAP(4,j) = -1;
+    else
+        EAP(4,j) = numer/denom;
+    end
+end
