55f8cc62ba01a546794ee1df85b4783f7ade823b
Haxonek <thomash1996@live.com>
2017-12-19 21:18:08 -0600

    More changes, textual improvments mostly

--- isAuthors.m
+++ isAuthors.m
@@ -1,22 +1,14 @@
 function percentage = isAuthors(sentences, words, w, y_expected)
-    sizen = length(sentences)
+    sizen = length(sentences);
     tmp = zeros(1,sizen);
-    
-%     tmp_cell = sentences';
-%     sentences = cell(50,3);
-%     sentences(:,2) = tmp_cell;
-    size(sentences)
-%     
-%     sentences{1:5,2};
-    
+    % size used for bug checking
+    size(sentences);
     for i = 1:sizen
        % we run the function on each sentence
-%        i
-%        sentences{i,:}
        tmp(i) = isAuthor(words, cell2mat(sentences{i,2}), w);
     end
     
-    tmp(1:5)
+    tmp(1:2)
     
     newSum = 0;
     denom = 0;
@@ -28,22 +20,4 @@ function percentage = isAuthors(sentences, words, w, y_expected)
     end
     
     percentage = newSum/denom;
-
-%     sizen = length(sentences);
-%     tmp = zeros(1,sizen);
-%     for i = 1:sizen
-%        % we run the function on each sentence
-%        tmp(i) = isAuthor(words, sentences(i,:), w);
-%     end
-% 
-%     newSum = 0;
-%     denom = 0;
-%     for i = 1:sizen;
-%         denom = denom + 1;
-%         if (sign(y_expected(i)) == sign(tmp(1,i)))
-%             newSum = newSum + 1;
-%         end
-%     end
-%     
-%     percentage = newSum/denom;
 end
